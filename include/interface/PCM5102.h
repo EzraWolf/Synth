d71efe7ba94bdaf5818cdc6a583fa6221afc6dc3
EzraWolf <wolfezraw@gmail.com>
2023-03-05 22:17:47 -0500

    Updated function definitions

--- include/interface/PCM5102.h
+++ include/interface/PCM5102.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 void PCM5102_init(bool is_mono); // Is 32b audio resolution by default
-void PCM5102_update_freq(uint32_t freq);
+void PCM5102_update_freq(uint32_t freq_l, uint32_t freq_r);
+void PCM5102_update_vol(uint8_t vol);
 
 #endif // _INTERFACE_PCM5102_H_
--- src/interface/PCM5102.c
+++ src/interface/PCM5102.c
@@ -6,10 +6,46 @@
 // Local
 #include "pio/PCM5102.pio.h" // The "compiled" PCM5102.pio file
 
-void PCM5102_init(uint8_t bit_resolution, bool is_mono) {
+/**
+ * @name PCM5102_init
+ * @date 05-Mar-23
+ * 
+ * @brief Initializes the PCM5102 audio DAC. The default bit resolution is 32 bits.
+ * If `is_mono` is set to false, then the Pico sends out 64b total, 32b each for
+ * the left and right channels.
+ * 
+ * @param bool is_mono
+ * @return void
+ */
+void PCM5102_init(bool is_mono) {
 
 }
 
-void PCM5102_update_freq(uint32_t freq) {
+/**
+ * @name PCM5102_update_freq
+ * @date 05-Mar-23
+ * 
+ * @brief Updates the output frequency, and holds the last set frequency if not updated.
+ * If the device is setup as mono, then `freq_r` isn't read and can be set to anything,
+ * but it's recommended to set it to 0 regardless
+ * 
+ * 
+ * @param uint32_t freq
+ * @return void
+ */
+void PCM5102_update_freq(uint32_t freq_l, uint32_t freq_r) {
+
+}
+
+/**
+ * @name PCM5102_update_vol
+ * @date 05-Mar-23
+ * 
+ * @brief Updates the volume of the PCA5102. Holds the last set volume if not udpated
+ * 
+ * @param uint8_t vol
+ * @return void
+ */
+void PCM5102_update_vol(uint8_t vol) {
 
 }
